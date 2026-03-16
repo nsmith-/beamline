Fix some issues in line_cylinder_intersection, WIP gradient

src/beamline/jax/geometry.py
@@ -62,28 +62,34 @@ def line_cylinder_intersection(
         (t, h): Time of intersection and z coordinate of the intersection point
 
     Following the formulas of https://en.wikipedia.org/wiki/Line-cylinder_intersection
+    (with the cylinder radius absorbed into the axis vector for convenience)
     """
     a = cyl_axis
     b = cyl_point - ray.p
     n = ray.t
-    nxa = n.cross(a)
-    a2 = a.dot(a)
-    discriminant = nxa.dot(nxa) - a2 * (b.dot(nxa)) ** 2
+    # assuming a.a = r^2,
+    # ax(n*d - b) . ax(n*d - b) = (a.a)^2
+    # d^2 axn . axn - 2d axb . axn + (axb . axb - (a.a)^2) = 0
+    # d = (axb . axn  +- sqrt( (axb . axn)^2 - axn.axn * (axb . axb - (a.a)^2) )) / axn . axn
+    axn = a.cross(n)
+    axb = a.cross(b)
+    r2 = a.dot(a)
+    discriminant = axb.dot(axn) ** 2 - axn.dot(axn) * (axb.dot(axb) - r2 * r2)
     sd = jnp.where(discriminant >= 0, jnp.sqrt(jnp.abs(discriminant)), jnp.inf)
-    t1num = nxa.dot(b.cross(a)) + sd
-    t2num = nxa.dot(b.cross(a)) - sd
-    tden = nxa.dot(nxa)
+    t1num = axb.dot(axn) + sd
+    t2num = axb.dot(axn) - sd
+    tden = axn.dot(axn)
     t = jnp.where(
         tden == 0.0,
         jnp.inf,
         jnp.where(abs(t1num) <= abs(t2num), t1num, t2num)
         / jnp.where(tden == 0.0, 1.0, tden),
     )
-    # if vec.t has zeros, then inf * 0 causes trouble, so work around it
+    # if n has zeros, then inf * 0 causes trouble, so work around it
     h = jnp.where(
         t == jnp.inf,
         jnp.inf,
-        a.dot(jnp.where(t == jnp.inf, 0.0, t) * ray.t - cyl_point) / a2,
+        a.dot(jnp.where(t == jnp.inf, 0.0, t) * n - b) / jnp.sqrt(r2),
     )
     return t, h
 

test/jax/test_cooling_benchmarks.py
@@ -15,7 +15,8 @@ import pytest
 from matplotlib import pyplot as plt
 from matplotlib.animation import FuncAnimation
 
-from beamline.jax.coordinates import Cartesian3, Cartesian4
+from beamline.jax.coordinates import Cartesian3, Cartesian4, Transform
+from beamline.jax.emfield import EMTensorField, TransformEMField
 from beamline.jax.integrate.propagate import diffrax_solve, particle_interaction
 from beamline.jax.kinematics import MuonStateDz
 from beamline.jax.magnet.solenoid import ThickSolenoid
@@ -468,3 +469,125 @@ def test_benchmark_3p3_rf_cavity(artifacts_dir, benchmark):
 
     bench_func()
     benchmark(bench_func)
+
+
+@pytest.mark.parametrize("forward", [True, False])
+def test_benchmark_3p3_rf_tune(artifacts_dir, forward: bool):
+    """Derivative with respect to cavity phase and z position
+
+    Both should be able to accomplish the same thing if gradients are working
+
+    Test that forward and reverse mode autodiff work (TODO: and give the same result)
+    """
+    frequency = 704.0 * u.MHz
+    # time to get to 0 position is 500mm / beta*c
+    ref_muon = MuonStateDz.make(
+        position=Cartesian4.make(),
+        momentum=Cartesian3.make(z=200.0 * u.MeV),
+        q=1,
+    )
+    t0 = 500 * u.mm / (ref_muon.beta() * u.c_light)
+    # advance by pi/2 so we are in bunching mode
+    # i.e. refence particle momentum is unchanged
+    phase = -2 * u.pi * ((t0 * frequency - 0.25) % 1.0)
+
+    def build_cavity(phase_shift: SFloat, z_shift: SFloat) -> EMTensorField:
+        cavity = PillboxCavity(
+            length=183.6 * u.mm,
+            frequency=frequency,
+            E0=30.0 * u.MV / u.m,
+            mode="TM",
+            m=0,
+            n=1,
+            p=0,
+            phase=phase + phase_shift,
+        )
+        return TransformEMField(
+            transform=Transform.make_translation(z=z_shift),
+            field=cavity,
+        )
+
+    def track_ref(field: EMTensorField) -> MuonStateDz:
+        start = MuonStateDz.make(
+            position=Cartesian4.make(x=1e-8 * u.mm, z=-500.0 * u.mm),
+            momentum=Cartesian3.make(z=200.0 * u.MeV),
+            q=1,
+        )
+        saveat = jnp.array([-500.0 * u.mm, 500.0 * u.mm])
+        ys, _ = diffrax_solve(
+            field=field,
+            start=start,
+            cts=saveat,
+            rtol=1e-7,
+            atol=1e-9,
+            forward_mode=forward,
+        )
+        return jax.tree.map(lambda x: x[-1], ys)
+
+    @jax.jit
+    def objective(phase_shift: SFloat, z_shift: SFloat) -> SFloat:
+        field = build_cavity(phase_shift, z_shift)
+        end = track_ref(field)
+        return end.kin.t.ct
+
+    jacobian = jax.jacfwd if forward else jax.jacrev
+
+    phase_vals = jnp.linspace(-1.5, 1.5, 9)
+    E_vals = []
+    dEdphase_vals = []
+    for phase_shift in phase_vals:
+        Efinal = objective(phase_shift, 0.0)
+        dEdphase = jacobian(objective, argnums=0)(phase_shift, 0.0)
+        E_vals.append(Efinal)
+        dEdphase_vals.append(dEdphase)
+
+    fig, ax = plt.subplots()
+
+    ax.plot(
+        phase_vals,
+        E_vals,
+        marker="o",
+        color="green",
+        lw=1,
+    )
+    for phase_shift, Efinal, dEdphase in zip(
+        phase_vals, E_vals, dEdphase_vals, strict=True
+    ):
+        p = jnp.linspace(phase_shift - 0.05, phase_shift + 0.05, 3)
+        ax.plot(p, Efinal + dEdphase * (p - phase_shift), color="black")
+
+    ax.set_xlabel("Phase shift [rad]")
+    ax.set_ylabel("Final energy [MeV]")
+    fig.savefig(artifacts_dir / "benchmark_3p3_rf_phase_tune.png")
+
+    if not forward:
+        return pytest.xfail(reason="Reverse mode for z not working due to nans")
+
+    z_vals = jnp.linspace(-150, 150, 9)
+    E_vals = []
+    dEdz_vals = []
+    for z_shift in z_vals:
+        Efinal = objective(0.0, z_shift)
+        dEdz = jacobian(objective, argnums=1)(0.0, z_shift)
+        E_vals.append(Efinal)
+        dEdz_vals.append(dEdz)
+
+    fig, ax = plt.subplots()
+
+    ax.plot(
+        z_vals,
+        E_vals,
+        marker="o",
+        color="green",
+        lw=1,
+    )
+    for z_shift, Efinal, dEdz in zip(z_vals, E_vals, dEdz_vals, strict=True):
+        p = jnp.linspace(z_shift - 5, z_shift + 5, 3)
+        ax.plot(p, Efinal + dEdz * (p - z_shift), color="black")
+
+    ax.set_xlabel("z shift [mm]")
+    ax.set_ylabel("Final energy [MeV]")
+    fig.savefig(artifacts_dir / "benchmark_3p3_rf_z_tune.png")
+
+    # Efinal, (dE_dphase, dE_dz) = objective(0.0, 0.0)
+    # raise RuntimeError(f"Efinal={Efinal}, dE/dphase={dE_dphase}, dE/dz={dE_dz}")

test/jax/test_geometry.py
@@ -1,8 +1,13 @@
+import jax
 import jax.numpy as jnp
 import pytest
 
 from beamline.jax.coordinates import Cartesian3, Tangent
 from beamline.jax.geometry import line_cylinder_intersection, line_plane_intersection
+
+
+def approx(val):
+    return pytest.approx(val, rel=1e-15, abs=1e-15)
 
 
 def test_line_plane_intersection():
@@ -15,18 +20,18 @@ def test_line_plane_intersection():
         t=Cartesian3.make(z=1.0),
     )
     t, u, v = line_plane_intersection(ray, pcenter, pu, pv)
-    assert t == pytest.approx(1.0, rel=1e-15)
-    assert u == pytest.approx(0.0, rel=1e-15)
-    assert v == pytest.approx(0.0, rel=1e-15)
+    assert t == approx(1.0)
+    assert u == approx(0.0)
+    assert v == approx(0.0)
 
     ray = Tangent(
         p=Cartesian3.make(z=-1.0),
         t=Cartesian3.make(x=1.0, z=1.0),
     )
     t, u, v = line_plane_intersection(ray, pcenter, pu, pv)
-    assert t == pytest.approx(1.0, rel=1e-15)
-    assert u == pytest.approx(1.0, rel=1e-15)
-    assert v == pytest.approx(0.0, rel=1e-15)
+    assert t == approx(1.0)
+    assert u == approx(1.0)
+    assert v == approx(0.0)
 
     ray = Tangent(
         p=Cartesian3.make(z=-1.0),
@@ -38,6 +43,36 @@ def test_line_plane_intersection():
     assert v == -jnp.inf  # must be an artifact of the algorithm
 
 
+def test_line_plane_intersection_grad():
+    def ray_plane(plane_z):
+        pcenter = Cartesian3.make(z=plane_z)
+        pu = Cartesian3.make(x=1.0, z=plane_z)
+        pv = Cartesian3.make(y=1.0, z=plane_z)
+        ray = Tangent(
+            p=Cartesian3.make(z=-1.0),
+            t=Cartesian3.make(x=1.0, z=1.0),
+        )
+        t, u, v = line_plane_intersection(ray, pcenter, pu, pv)
+        return t, u, v
+
+    t, u, v = ray_plane(0.0)
+    assert t == approx(1.0)
+    assert u == approx(1.0)
+    assert v == approx(0.0)
+
+    dt_dz, du_dz, dv_dz = jax.jacfwd(ray_plane)(0.0)
+    assert dt_dz == approx(1.0)
+    assert du_dz == approx(1.0)
+    assert dv_dz == approx(0.0)
+
+    dt_dz, du_dz, dv_dz = jax.jacrev(ray_plane)(0.0)
+    assert dt_dz == approx(1.0)
+    assert du_dz == approx(1.0)
+    assert dv_dz == approx(0.0)
+
+    # TODO: test more exotic cases
+
+
 def test_line_cylinder_intersection():
     cyl_point = Cartesian3.make()
     cyl_axis = Cartesian3.make(x=1.0)
@@ -47,24 +82,24 @@ def test_line_cylinder_intersection():
         t=Cartesian3.make(y=1.0),
     )
     t, h = line_cylinder_intersection(ray, cyl_point, cyl_axis)
-    assert t == pytest.approx(0.0, abs=1e-15)
-    assert h == pytest.approx(0.0, rel=1e-15)
+    assert t == approx(0.0)
+    assert h == approx(0.0)
 
     ray = Tangent(
         p=Cartesian3.make(z=0.1),
         t=Cartesian3.make(z=1.0),
     )
     t, h = line_cylinder_intersection(ray, cyl_point, cyl_axis)
-    assert t == pytest.approx(0.9, rel=1e-15)
-    assert h == pytest.approx(0.0, rel=1e-15)
+    assert t == approx(0.9)
+    assert h == approx(0.0)
 
     ray = Tangent(
         p=Cartesian3.make(),
         t=Cartesian3.make(x=2.0, y=2.0),
     )
     t, h = line_cylinder_intersection(ray, cyl_point, cyl_axis)
-    assert t == pytest.approx(0.5, rel=1e-15)
-    assert h == pytest.approx(1.0, rel=1e-15)
+    assert t == approx(0.5)
+    assert h == approx(1.0)
 
     ray = Tangent(
         p=Cartesian3.make(x=0.1, y=-1.0),
@@ -73,3 +108,63 @@ def test_line_cylinder_intersection():
     t, h = line_cylinder_intersection(ray, cyl_point, cyl_axis)
     assert t == jnp.inf
     assert h == jnp.inf
+
+    cyl_axis = Cartesian3.make(z=3.0)
+    ray = Tangent(
+        p=Cartesian3.make(y=0.5, z=1.0),
+        t=Cartesian3.make(y=1.0),
+    )
+    t, h = line_cylinder_intersection(ray, cyl_point, cyl_axis)
+    assert t == approx(2.5)
+    assert h == approx(1.0)
+
+
+def test_line_cylinder_intersection_grad():
+    def ray_cylinder(cyl_radius):
+        cyl_point = Cartesian3.make()
+        cyl_axis = Cartesian3.make(x=cyl_radius)
+        ray = Tangent(
+            p=Cartesian3.make(y=0.25),
+            t=Cartesian3.make(y=1.0),
+        )
+        t, h = line_cylinder_intersection(ray, cyl_point, cyl_axis)
+        return t, h
+
+    t, h = ray_cylinder(1.0)
+    assert t == approx(0.75)
+    assert h == approx(0.0)
+
+    dt_dr, dh_dr = jax.jacfwd(ray_cylinder)(1.0)
+    assert dt_dr == approx(1.0)
+    assert dh_dr == approx(0.0)
+
+    dt_dr, dh_dr = jax.jacrev(ray_cylinder)(1.0)
+    assert dt_dr == approx(1.0)
+    assert dh_dr == approx(0.0)
+
+    def ray_cylinder(cyl_x):
+        cyl_point = Cartesian3.make(x=cyl_x)
+        cyl_axis = Cartesian3.make(x=1.0)
+        ray = Tangent(
+            p=Cartesian3.make(x=-1.0),
+            t=Cartesian3.make(x=1.0),
+        )
+        t, h = line_cylinder_intersection(ray, cyl_point, cyl_axis)
+        return t, h
+
+    t, h = ray_cylinder(0.0)
+    assert t == jnp.inf
+    assert h == jnp.inf
+
+    return pytest.xfail(
+        reason="When parallel, the auto-diff is not handling inf correctly"
+    )
+    # TODO: is grad 0 the right behavior?
+
+    dt_dx, dh_dx = jax.jacfwd(ray_cylinder)(0.0)
+    assert dt_dx == 0.0
+    assert dh_dx == 0.0
+
+    dt_dx, dh_dx = jax.jacrev(ray_cylinder)(0.0)
+    assert dt_dx == 0.0
+    assert dh_dx == 0.0